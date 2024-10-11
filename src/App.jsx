d6547d02ccfac44f65d2cf3019082dd8ae995e17
Mohit kumar <mohitdcsa.com>
2024-10-11 22:58:09 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer.jsx"
+
+function App() {
+
+  return (
+    <div className="font-Manrope">
+      <Navbar/>
+      <Footer/>
+    </div >
+    
+
+  );
+};
+
+export default App;
